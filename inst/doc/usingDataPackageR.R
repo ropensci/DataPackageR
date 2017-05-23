4afa1017f99791560348037f3d0ba9909af33ac2
Greg Finak <greg.finak@gmail.com>
2017-05-23 15:19:08 -0700

    Update vignette

---
+++ inst/doc/usingDataPackageR.R
@@ -0,0 +1,12 @@
+## ---- echo = FALSE-------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval=FALSE
+)
+
+## ------------------------------------------------------------------------
+#  library(DataPackageR)
+#  setwd("/tmp")
+#  DataPackageR::datapackage.skeleton("MyNewStudy",force=TRUE,code_files = c("/tmp/MungeDataset1.Rmd","/tmp/MungeDataset2.Rmd"),r_object_names = c("dataset1","dataset2"))
+
