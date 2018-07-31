78031e1e855d063e79125f9aa987d8fdb61153ca
gfinak <greg.finak@gmail.com>
2018-07-31 08:11:42 -0700

    testing

--- R/processData.R
+++ R/processData.R
@@ -272,6 +272,7 @@ DataPackageR <- function(arg = NULL, deps = TRUE) {
         knitr::spin(r_files[i], precious = TRUE,
                     knit = FALSE)
         r_files[i] <- gsub("\\.r","\\.Rmd",tolower(r_files[i]))
+        cat(r_files[i])
         lines <- readLines(r_files[i])
         lines <- c("---",
               paste0("title: ",basename(r_files[i])),
