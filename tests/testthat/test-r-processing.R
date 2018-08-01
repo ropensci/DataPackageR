308e8cb3076fba1dd0c44c3666390e6a1224839c
gfinak <greg.finak@gmail.com>
2018-08-01 12:20:15 -0700

    Tests for R vignettes with and without yaml header.

--- tests/testthat/test-r-processing.R
+++ tests/testthat/test-r-processing.R
@@ -1,5 +1,5 @@
 context("R file script processing to vignette")
-test_that("R file processing works", {
+test_that("R file processing works and creates vignettes", {
   file <- system.file("extdata", "tests", "rfileTest.R",
                       package = "DataPackageR")
   datapackage_skeleton(
@@ -14,10 +14,31 @@ test_that("R file processing works", {
       package_build(
         file.path(tempdir(), "rfiletest"))),
     "rfiletest_1.0.tar.gz")
-  v <- vignette(package="rfiletest")
+  v <- vignette(package = "rfiletest")
   expect_equal(v$results[,"Item"],"rfileTest")
   
   unlink(file.path(tempdir(), "rfiletest"),
          recursive = TRUE,
          force = TRUE)
+  
+  file <- system.file("extdata", "tests", "rfileTest_noheader.R",
+                      package = "DataPackageR")
+  datapackage_skeleton(
+    name = "rfiletest",
+    path = tempdir(),
+    code_files = c(file),
+    force = TRUE,
+    r_object_names = "data"
+  )
+  expect_equal(
+    basename(
+      package_build(
+        file.path(tempdir(), "rfiletest"))),
+    "rfiletest_1.0.tar.gz")
+  v <- vignette(package="rfiletest")
+  expect_equal(v$results[,"Item"],"rfileTest_noheader")
+  unlink(file.path(tempdir(), "rfiletest"),
+         recursive = TRUE,
+         force = TRUE)
+  
 })
