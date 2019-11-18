c53454f53743523e4b9f2d51ce8705dd3f0831ad
gfinak <greg.finak@gmail.com>
2019-11-18 09:53:42 -0800

    Patch to address #73

--- tests/testthat/test-ignore.R
+++ tests/testthat/test-ignore.R
@@ -13,6 +13,9 @@ test_that("use_ignore works", {
       r_object_names = c("cars_over_20")
     )
   )
-  expect_output(use_ignore(file = "mydata.csv",path = "inst/extdata"),"Adding 'mydata.csv' to 'inst/extdata/\\.gitignore'|Adding '\\^inst/extdata/mydata\\\\.csv\\$' to '\\.Rbuildignore'")
+  #expect_output(use_ignore(file = "mydata.csv",path = "inst/extdata"),"Adding 'mydata.csv' to 'inst/extdata/\\.gitignore'|Adding '\\^inst/extdata/mydata\\\\.csv\\$' to '\\.Rbuildignore'")
+  withr::with_options(c(crayon.enabled = FALSE), {
+	expect_message(use_ignore(file = "mydata.csv", path = "inst/extdata"), "Adding 'mydata.csv' to 'inst/extdata/\\.gitignore'|Adding '\\^inst/extdata/mydata\\.csv\\$' to '\\.Rbuildignore'")
+  })
   expect_message(use_ignore(),"No file name provided to ignore.")
 })
