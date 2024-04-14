6c505d0be4240a8c52233b0e8cbc981e2205a658
Dave Slager <slager@users.noreply.github.com>
2024-04-13 20:42:13 -0700

    now done better in tests/testthat/setup.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,6 @@
 library(testthat)
 library(DataPackageR)
-options("DataPackageR_interact" = FALSE)
 # Test only if pandoc is available.
 if (rmarkdown::pandoc_available()) {
   test_check("DataPackageR")
 }
-options("DataPackageR_interact" = interactive())
