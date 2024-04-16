e6af304dd92302f920312764f7cb5ebbaae3f639
Dave Slager <slager@users.noreply.github.com>
2024-04-16 00:20:29 -0700

    fix test that relied on console output

--- tests/testthat/test-ignore.R
+++ tests/testthat/test-ignore.R
@@ -4,18 +4,28 @@ test_that("use_ignore works", {
   file <- system.file("extdata", "tests", "subsetCars.Rmd",
                       package = "DataPackageR"
   )
-  expect_null(
-    datapackage_skeleton(
-      name = "subsetCars",
-      path = tempdir(),
-      code_files = c(file),
-      force = TRUE,
-      r_object_names = c("cars_over_20")
+  local({
+    tempdir <- withr::local_tempdir()
+    expect_null(
+      datapackage_skeleton(
+        name = "subsetCars",
+        path = tempdir,
+        code_files = c(file),
+        force = TRUE,
+        r_object_names = c("cars_over_20")
+      )
     )
-  )
-  #expect_output(use_ignore(file = "mydata.csv",path = "inst/extdata"),"Adding 'mydata.csv' to 'inst/extdata/\\.gitignore'|Adding '\\^inst/extdata/mydata\\\\.csv\\$' to '\\.Rbuildignore'")
-  withr::with_options(c(crayon.enabled = FALSE), {
-	expect_message(use_ignore(file = "mydata.csv", path = "inst/extdata"), "Adding 'mydata.csv' to 'inst/extdata/\\.gitignore'|Adding '\\^inst/extdata/mydata\\.csv\\$' to '\\.Rbuildignore'")
+    use_ignore(file = "mydata.csv", path = file.path("inst", "extdata"))
+    expect_true(
+      'mydata.csv' %in% readLines(
+        file.path(tempdir, 'subsetCars', 'inst', 'extdata', '.gitignore')
+      )
+    )
+    expect_true(
+      '^inst/extdata/mydata\\.csv$' %in% readLines(
+        file.path(tempdir, 'subsetCars', '.Rbuildignore')
+      )
+    )
+    expect_message(use_ignore(),"No file name provided to ignore.")
   })
-  expect_message(use_ignore(),"No file name provided to ignore.")
 })
