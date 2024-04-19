adaaa30dd65e75b7761bdda744b3449b7cfca004
Dave Slager <slager@users.noreply.github.com>
2024-04-18 19:19:48 -0700

    error out if pkg dirname is not same as pkg name, other code assumes it

--- tests/testthat/test-build-locations.R
+++ tests/testthat/test-build-locations.R
@@ -31,3 +31,14 @@ test_that("package can be built from different locations", {
     force = TRUE
   )
 })
+
+test_that("Error on data pkg dirname different from data pkg name", {
+  td <- withr::local_tempdir()
+  sn <- 'skelname'
+  not_sn <- paste0('not_', sn)
+  datapackage_skeleton(name = sn, path = td)
+  file.rename(from = file.path(td, sn), to = file.path(td, not_sn))
+  err_msg <- paste("Data package name in DESCRIPTION does not match",
+                   "name of the data package directory")
+  expect_error(package_build(file.path(td, not_sn)), err_msg)
+})
