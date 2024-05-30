f3fe0181f26a589216f2bee29cf357f3805cc6a1
Dave Slager <slager@users.noreply.github.com>
2024-05-30 16:57:33 -0700

    respect verbose option set outside package

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(libname, pkgname) {
   # keeping this first option hardcoded on load for now
   options("DataPackageR_packagebuilding" = FALSE)
-  options("DataPackageR_verbose" = TRUE)
-  # respect previous user setting for 'DataPackageR_interact' if set
+  # respect previous user setting for options if set
   op <- options()
   op.DataPackageR <- list(
-    DataPackageR_interact = interactive()
+    DataPackageR_interact = interactive(),
+    DataPackageR_verbose = TRUE
   )
   toset <- !(names(op.DataPackageR) %in% names(op))
   if (any(toset)) options(op.DataPackageR[toset])
